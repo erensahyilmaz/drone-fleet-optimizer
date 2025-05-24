ff4d52bdf3fe274e507cc4f56ced9f98c7cada64
erensahyilmaz <152299536+erensahyilmaz@users.noreply.github.com>
2025-05-24 10:08:00 +0300

    main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,35 @@
-from src.utils.data_loader import load_dataset
+from src.utils import data_generator, data_loader
 
-# Veriyi yükle
-drones, deliveries, zones = load_dataset()
+def main():
+    # Veri üret
+    drones = data_generator.generate_drones(5)
+    deliveries = data_generator.generate_deliveries(20)
+    zones = data_generator.generate_no_fly_zones(2)
 
-# Verileri yazdır
-print("Drones:")
-for d in drones:
-    print(d)
+    # Veriyi dosyaya kaydet
+    data_generator.save_dataset_to_txt(drones, deliveries, zones)
 
-print("\nDeliveries:")
-for dp in deliveries:
-    print(dp)
+    print("✅ Veri başarıyla 'data/dataset.txt' dosyasına kaydedildi.")
 
-print("\nNo-Fly Zones:")
-for z in zones:
-    print(z)
+    # Dosyadan veriyi yükle
+    drones_loaded, deliveries_loaded, zones_loaded = data_loader.load_dataset()
+
+    print("✅ Dataset başarıyla yüklendi.")
+    print(f"Drone sayısı: {len(drones_loaded)}")
+    print(f"Teslimat sayısı: {len(deliveries_loaded)}")
+    print(f"No-fly zone sayısı: {len(zones_loaded)}")
+
+    print("Drones:")
+    for d in drones:
+        print(d)  # __repr__ metodu detaylı yazdıracak
+
+    print("\nDeliveries:")
+    for dp in deliveries:
+        print(dp)
+
+    print("\nNo-Fly Zones:")
+    for zone in zones:
+        print(zone)
+
+if __name__ == "__main__":
+    main()
